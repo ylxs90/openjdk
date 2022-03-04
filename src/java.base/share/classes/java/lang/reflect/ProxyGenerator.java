a60004b9e31cbe41d4e4745cb74765bac2a3cebb
Sergey Tsypanov <sergei.tsypanov@yandex.ru>
2022-03-04 22:24:34 +0200

    8282662: Use List/Set.of() factory methods to save memory

--- src/java.base/share/classes/java/lang/reflect/ProxyGenerator.java
+++ src/java.base/share/classes/java/lang/reflect/ProxyGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 1999, 2021, Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 1999, 2022, Oracle and/or its affiliates. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -724,7 +724,7 @@ final class ProxyGenerator extends ClassWriter {
 
             MethodVisitor mv = cw.visitMethod(accessFlags,
                     method.getName(), desc, null,
-                    typeNames(Arrays.asList(exceptionTypes)));
+                    typeNames(List.of(exceptionTypes)));
 
             int[] parameterSlot = new int[parameterTypes.length];
             int nextSlot = 1;
